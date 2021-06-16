b0b957da5248c71b87e1620cb24f898c61787d69
Sabeena <sabeenarayyan13@gmail.com>
2021-06-16 14:59:01 +0400

    added the final modification

--- script.js
+++ script.js
@@ -1,77 +1,85 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function () {
-   console.log("window loaded");
+   // console.log("window loaded");
    let form = this.document.querySelector("form");
    let letters = /^[A-Za-z]+$/;
    const pilotName = document.querySelector("input[name=pilotName]");
-      const copilotName = document.querySelector("input[name=copilotName]");
-      const cargoMass = document.querySelector("input[name=cargoMass]");
-      const fuelLevel = document.querySelector("input[name=fuelLevel]");
-      const div = document.getElementById("launchStatusCheck");
-      const launchStatus = document.getElementById("launchStatus");
-      const items = document.getElementById("faultyItems");
-      const fuelStatus = document.getElementById("fuelStatus");
-      const cargoStatus = document.getElementById("cargoStatus");
+   const copilotName = document.querySelector("input[name=copilotName]");
+   const cargoMass = document.querySelector("input[name=cargoMass]");
+   const fuelLevel = document.querySelector("input[name=fuelLevel]");
+   const div = document.getElementById("launchStatusCheck");
+   const launchStatus = document.getElementById("launchStatus");
+   const items = document.getElementById("faultyItems");
+   const pilotStatus = document.getElementById("pilotStatus");
+   const copilotStatus = document.getElementById("copilotStatus");
+   const fuelStatus = document.getElementById("fuelStatus");
+   const cargoStatus = document.getElementById("cargoStatus");
+
    form.addEventListener("submit", function (event) {
+
       //Validating all the input 
+
       if (pilotName.value === "" || copilotName.value === "" || cargoMass.value === "" || fuelLevel.value === "") {
          alert("All fields are required!");
       }
-      // validating the input type for cargomass and fuellevel
-      if (isNaN(cargoMass.value) == true || isNaN(fuelLevel.value) == true) {
+
+      // validating the input type for cargomass,fuellevel and text input
+
+      else if (isNaN(cargoMass.value) == true || isNaN(fuelLevel.value) == true) {
          alert("Make sure to enter valid information for each field!");
       }
-      // validating the text input
-      if (pilotName.value.match(letters) || copilotName.value.match(letters)) {
-         return true;
-      } else {
-         alert("Make sure to enter valid information for each field!")
+
+      else if (!pilotName.value.match(letters) || !copilotName.value.match(letters)) {
+         alert("Make sure to enter valid information for each field!");
       }
-     
-      //updating the faulty items
-      div.innerHTML = `
-      
-     <ul>
-     <li> Pilot ${pilotName.value} is ready for launch. </li>
-     <li> Co-pilot ${copilotName.value} is ready for launch</li>
-     
-     `;
-     // updating fuelstatus and cargostatus
-      if (fuelLevel < 10000 || cargoMass > 10000) {
+
+      //updating faultyitems,fuelstatus,cargostatus
+
+      if (fuelLevel.value < 10000) {     // UPDATING FUEL STATUS
          items.style.visibility = "visible";
-         fuelStatus.innerHTML = "There is no enough fuel for the journey";
-         cargoStatus.innerHTML = "There is too much mass for the shuttle to take-off";
-         launchStatus.innerHTML = "The shuttle not ready for launch";
-         launchStatus.style.backgroundColor = "red";
-      } else {
-         launchStatus.innerHTML = "The shuttle is ready for launch";
-         launchStatus.style.backgroundColor = "green";
+         pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch.`;
+         copilotStatus.innerHTML = `Co-pilot ${copilotName.value} is ready for launch`;
+         fuelStatus.innerHTML = `There is no enough fuel for the journey`;
+         launchStatus.innerHTML = `The shuttle not ready for launch`;
+         launchStatus.style.color = "red";
+      } else if (cargoMass.value > 10000) {    // UPDATING CARGO STATUS
+         items.style.visibility = "visible";
+         pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch.`;
+         copilotStatus.innerHTML = `Co-pilot ${copilotName.value} is ready for launch`;
+         cargoStatus.innerHTML = `There is too much mass for the shuttle to take-off`;
+         launchStatus.innerHTML = `The shuttle not ready for launch.`;
+         launchStatus.style.color = "red";
+      } else {            // IF THE SHUTTLE IS GOOD TO GO 
+         items.style.visibility = "hidden";
+         launchStatus.innerHTML = `The shuttle is ready for launch`;
+         launchStatus.style.color = "green";
       }
       event.preventDefault();
    });
-   });
-      // planetary json
-      fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
-         response.json().then(function (json) {
-            console.log(json);
-            let mission = document.getElementById("missionTarget");
-            mission.innerHTML = `
-            
-  <h2>Mission Destination</h2>
-  
-  <ol>
-   <li>Name: ${json[3].name}</li>
-   <li>Diameter: ${json[3].diameter}</li>
-   <li>Star: ${json[3].star}</li>
-   <li>Distance from Earth: ${json[3].distance}</li>
-   <li>Number of Moons: ${json[3].moons}</li>
-</ol>
-<img src="${json[3].image}"> 
+});
+// FETCHING PLANETARY JSON AND BONUS MISSION
+fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
+   response.json().then(function (json) {
+      // console.log(json);
+      let randomValue = json[parseInt(Math.random() * json.length)];
+      // console.log(randomValue);
+      let mission = document.getElementById("missionTarget");
 
+      //UPDATING THE HTML DYNAMICALLY
+      mission.innerHTML = `
+            <h2>Mission Destination</h2>
+            <ol>
+   <li>Name: ${randomValue.name}</li>
+   <li>Diameter: ${randomValue.diameter}</li>
+   <li>Star: ${randomValue.star}</li>
+   <li>Distance from Earth: ${randomValue.distance}</li>
+   <li>Number of Moons: ${randomValue.moons}</li>
+</ol>
+<img src="${randomValue.image}"> 
 `;
- });
-      });
-  
-   
+   });
+});
+
+
 
 
